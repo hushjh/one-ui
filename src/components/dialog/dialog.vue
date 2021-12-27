07e8cacf071d6bbee4aaabbb2e4fa4c87aba4bd0
wangshz <wangshz@yuanian.com>
2021-12-27 15:22:39 +0800

    dialog打开关闭动画效果

--- src/components/dialog/dialog.vue
+++ src/components/dialog/dialog.vue
@@ -1,24 +1,30 @@
 <template>
-  <div
-    v-show="visible"
-    class="el-dialog__wrapper"
-    :style="style"
-    @click.self="handleWrapperClick"
+  <transition
+    name="dialog-fade"
+    @after-enter="afterEnter"
+    @after-leave="afterLeave"
   >
-    <div class="el-dialog">
-      <div class="el-dialog__header">
-        <span class="el-dialog__title">{{title}}</span>
-      </div>
-      <div class="el-dialog__body">
-        <slot>
-          <span>这是一段信息</span>
-        </slot>
-      </div>
-      <div class="el-dialog__footer" v-if="$slots.footer">
-        <slot name="footer"></slot>
+    <div
+      v-show="visible"
+      class="el-dialog__wrapper"
+      :style="style"
+      @click.self="handleWrapperClick"
+    >
+      <div class="el-dialog">
+        <div class="el-dialog__header">
+          <span class="el-dialog__title">{{title}}</span>
+        </div>
+        <div class="el-dialog__body">
+          <slot>
+            <span>这是一段信息</span>
+          </slot>
+        </div>
+        <div class="el-dialog__footer" v-if="$slots.footer">
+          <slot name="footer"></slot>
+        </div>
       </div>
     </div>
-  </div>
+  </transition>
 </template>
 <script lang="ts">
 import Vue from "vue";
@@ -72,7 +78,12 @@ export default class Dialog extends Mixins(Vue, Popup) {
     if (this.width) style.width = this.width;
     return style;
   }
-
+  afterEnter() {
+    this.$emit("opened");
+  }
+  afterLeave() {
+    this.$emit("closed");
+  }
   handleWrapperClick() {
     if (!this.closeOnClickModal) return;
     this.handClose();
